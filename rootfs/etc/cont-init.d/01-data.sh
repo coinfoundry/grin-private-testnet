7cad909a19215b289464989a2accf98efa18778e
Oliver Weichhold <oliver@weichhold.com>
2019-05-21 19:55:12 +0200

    WIP

--- rootfs/etc/cont-init.d/01-data.sh
+++ rootfs/etc/cont-init.d/01-data.sh
@@ -1,2 +1,13 @@
 #!/bin/sh
 
+if [ ! -d /data ]; then
+    mkdir /data
+fi
+
+if [ ! -d /data/node-1 ]; then
+    mkdir /data/node-1
+fi
+
+if [ ! -d /data/node-2 ]; then
+    mkdir /data/node-2
+fi
